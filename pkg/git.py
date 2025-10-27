d8fb60247508ebf946a934d745c76e6191ff2964
Vand0525 <vand0525@algonquinlive.com>
2025-10-27 09:57:06 -0400

    [refactor] Refactor git module for improved clarity and functionality.
    Changes:
    - Renamed get_diff to get_unstaged_diff for clarity.
    - Added get_staged_diff to retrieve staged changes.
    - Introduced get_unstaged_paths to fetch unstaged file paths.
    - Updated stage function to accept paths as an argument.
    - Added unstage_all function to reset the git index.
    - Refactored the main function to handle extended diff processing and staging.

--- pkg/git.py
+++ pkg/git.py
@@ -1,16 +1,37 @@
 import subprocess
 
-def get_diff():
+def get_unstaged_diff():
     result = subprocess.run(
-        ["git", "diff"], 
-        capture_output=True,
-        text=True,
-        check=False
+        ["git", "diff"], capture_output=True, text=True, check=False
     )
     return result.stdout
 
-def stage():
-    subprocess.run(['git', 'add', '.'])
+
+def get_staged_diff():
+    result = subprocess.run(
+        ["git", "diff", "--cached"], capture_output=True, text=True, check=False
+    )
+    return result.stdout
+
+
+def get_unstaged_paths():
+    result = subprocess.run(
+        ["git", "diff", "--name-only", "-z"],
+        capture_output=True,
+        text=False,
+        check=True,
+    )
+    items = result.stdout.split(b"\x00")
+    return [i.decode() for i in items if i]
+
+
+def stage(paths: list[str]):
+    subprocess.run(["git", "add", *paths])
+
+
+def unstage_all():
+    subprocess.run(["git", "reset"], check=True, stdout=subprocess.DEVNULL,)
+
 
 def commit(message: str):
-    subprocess.run(['git', 'commit', '-m', message])
+    subprocess.run(["git", "commit", "-m", message])
--- pkg/main.py
+++ pkg/main.py
@@ -1,13 +1,27 @@
 import click
-from pkg.llm import generate_commit_message
-from pkg.git import get_diff, stage, commit
+from pkg.llm import generate_commit_message, generate_staging_groups
+from pkg.git import get_unstaged_diff, get_staged_diff, get_unstaged_paths, unstage_all, stage, commit
 
 @click.command()
 @click.argument('prompt', required=False)
 def main(prompt):
-  diff = get_diff()
-  commit_message = generate_commit_message(prompt, diff)
-  stage()
-  commit(commit_message)
-  click.echo(f"Committed with message:\n{commit_message}")
+  unstage_all()
+  unstaged_diff = get_unstaged_diff()
+  unstaged_paths = get_unstaged_paths()
+  staging_groups = generate_staging_groups(unstaged_diff, unstaged_paths)
+  process_staging_groups(prompt, staging_groups)
+
+def process_staging_groups(prompt: str | None, staging_groups: list[dict]):
+  for i, group in enumerate(staging_groups, start=1):
+    type = group["type"]
+    paths = group["paths"]
+
+    stage(paths)
+    diff = get_staged_diff()
+
+    message = f"[{type}] {generate_commit_message(prompt, type, diff)}"
+    click.echo(f"\nCommit #{i}\n{message}\n")
   
+    commit(message)
+
+
