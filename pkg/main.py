1137d9e5c327ef79a16e75a7f56ccaf7d883922b
Vand0525 <vand0525@algonquinlive.com>
2025-10-28 18:52:19 -0400

    [refactor] Refactor the command-line interface structure.
    Changes:
    - Changed main function to act as a command under a new command group 'cli'
    - Added a new command group definition for better organization
    - Updated the `config` command to be part of the 'cli' group
    - Added a main guard to execute the CLI when the script runs

--- pkg/main.py
+++ pkg/main.py
@@ -3,16 +3,23 @@ from pkg.llm import generate_commit_message, generate_staging_groups
 from pkg.git import get_unstaged_diff, get_staged_diff, get_unstaged_paths, unstage_all, stage, commit
 from pkg.utils import process_staging_groups
 
-@click.command()
+@click.group()
+def cli():
+  pass
+
+@cli.command()
 @click.argument('prompt', required=False)
-def main(prompt):
+def commit(prompt):
   unstage_all()
   unstaged_diff = get_unstaged_diff()
   unstaged_paths = get_unstaged_paths()
   staging_groups = generate_staging_groups(unstaged_diff, unstaged_paths)
   process_staging_groups(prompt, staging_groups)
 
-@click.command()
+@cli.command()
 def config():
   click.echo('Configuring')
+
+if __name__ == '__main__':
+  cli()
 
